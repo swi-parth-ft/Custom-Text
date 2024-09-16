8f3f5b96b4bedf1ce8796d1d16f0d1439f3f1278
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2024-09-16 18:21:38 -0400

    Initial Commit

---
+++ Custom Text/Custom_TextApp.swift
@@ -0,0 +1,17 @@
+//
+//  Custom_TextApp.swift
+//  Custom Text
+//
+//  Created by Parth Antala on 9/16/24.
+//
+
+import SwiftUI
+
+@main
+struct Custom_TextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
